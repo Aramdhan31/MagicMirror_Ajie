56db732d1884737f2ffe40e0bd966bc7234f4a4d
Arjun Ramdhan <student@Arjuns-MacBook-Air.local>
2025-03-23 11:50:46 +0000

    fix: use Railway PORT and bind to 0.0.0.0 for cloud hosting

--- serveronly/index.js
+++ serveronly/index.js
@@ -2,7 +2,13 @@ const app = require("../js/app");
 const Log = require("../js/logger");
 
 app.start().then((config) => {
-	const bindAddress = config.address ? config.address : "localhost";
+	const port = process.env.PORT || config.port;
+	const bindAddress = config.address || "0.0.0.0";
+
+	config.port = port;
+	config.address = bindAddress;
+
 	const httpType = config.useHttps ? "https" : "http";
-	Log.info(`\n>>>   Ready to go! Please point your browser to: ${httpType}://${bindAddress}:${config.port}   <<<`);
+
+	Log.info(`\n>>>   Ready to go! Please point your browser to: ${httpType}://${bindAddress}:${port}   <<<`);
 });
